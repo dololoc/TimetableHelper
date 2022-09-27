47f4a0c15487969eb1139fd3a18ee1c0da301aca
dololoc <do-eunchae@do-eunchaeui-MacBookPro.local>
2022-09-27 12:21:32 +0900

    프론트

--- project-sido/src/App.tsx
+++ project-sido/src/App.tsx
@@ -13,21 +13,7 @@ function App() {
   return (
     <div className="App">
       <Header name="props.test"/>
-      {/* <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header> */}
-
+      
       <Login />
 
       <Signup />
